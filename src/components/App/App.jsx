51c2e9997d7592898771445ffe742089d9464c22
StruzhakS <StruzhakS@gmail.com>
2023-06-04 14:26:33 +0300

    do task 4-2 on hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,128 +1,98 @@
 import s from './style.module.css';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import DoForm from '../DoForm/DoForm';
 import Filter from '../FilterCOntacts/Filtercontacts';
 import ContactList from '../ContactList/ContactList';
 import ClearButton from 'components/ClearButton/ClearButton';
 
-class App extends Component {
-  state = {
-    contacts: [
+function App() {
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) || [
       { id: 'id-1', name: 'Julia Turkina', number: '050-111-86-46' },
       { id: 'id-6', name: 'Vova Turkin', number: '095-222-81-12' },
       { id: 'id-4', name: 'Kyivstar', number: '0-800-300-460' },
       { id: 'id-5', name: 'Ukrtelecom', number: '0-800-506-800' },
       { id: 'id-7', name: 'Serhii Struzhak', number: '0666-77-22-74' },
-    ],
-    filter: '',
-  };
-  clearContact = () => {
-    this.setState({
-      contacts: [],
-    });
+    ]
+  );
+  const [filter, setFilter] = useState('');
+
+  const clearContact = () => {
+    setContacts([]);
     return;
   };
-  componentDidMount() {
-    this.setState(({ contacts }) => ({
-      contacts: [
-        ...(JSON.parse(localStorage.getItem('contacts')) ||
-          this.state.contacts),
-      ],
-    }));
-  }
 
-  addContact = user => {
-    this.state.contacts.some(contact => {
+  const addContact = user => {
+    contacts.some(contact => {
       return contact.name.toLowerCase() === user.name.toLowerCase();
     })
       ? alert(`${user.name} is already in list`)
-      : this.setState(({ contacts }) => ({
-          contacts: [...contacts, user],
-        }));
+      : setContacts([...contacts, user]);
   };
 
-  phoneNumber = e => {
-    this.setState({
-      number: e.target.elements.value,
-    });
-    return;
+  const filterContacts = e => {
+    setFilter(e.target.value);
   };
 
-  filterContacts = e => {
-    this.setState({ filter: e.target.value });
-  };
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+    return () => {};
+  }, [contacts]);
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    // console.log(prevState.contacts);
-    // console.log(this.state.contacts);
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  getVisibleContacts = () => {
-    const { filter, contacts } = this.state;
+  const getVisibleContacts = () => {
     const normalizedContacts = filter.toLowerCase();
     return contacts.filter(contact => {
       return contact.name.toLowerCase().includes(normalizedContacts);
     });
   };
 
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(el => el.id !== contactId),
-    }));
+  const deleteContact = contactId => {
+    setContacts(prev => prev.filter(el => el.id !== contactId));
   };
-  onClickItem = e => {
+
+  const onClickItem = e => {
     if (e.target.nodeName !== 'BUTTON')
       console.dir('Тут має бути модалка)! (недопрацьовано) ');
   };
+  const visibleContacts = getVisibleContacts();
 
-  render() {
-    const { filter } = this.state;
-    const visibleContacts = this.getVisibleContacts();
-
-    return (
-      <div className={s.container}>
-        <div className={s.form}>
-          <h2>Phonebook</h2>
-          <div className={s.input}>
-            <DoForm
-              contacts={this.state.contacts}
-              addContact={this.addContact}
-            />
-          </div>
+  return (
+    <div className={s.container}>
+      <div className={s.form}>
+        <h2>Phonebook</h2>
+        <div className={s.input}>
+          <DoForm contacts={contacts} addContact={addContact} />
         </div>
-        <div>
-          {this.state.contacts.length ? <h2>Contacts</h2> : ''}
-          {this.state.contacts.length > 1 ? (
-            <Filter value={filter} filterContacts={this.filterContacts} />
-          ) : (
-            ''
-          )}
-          {this.state.contacts.length ? (
-            <ContactList
-              visibleContacts={visibleContacts}
-              deleteContact={this.deleteContact}
-              onClickItem={this.onClickItem}
-            />
-          ) : (
-            ''
-          )}
-        </div>
-        {(this.state.contacts.length === visibleContacts.length) &
-        (visibleContacts.length > 1) ? (
-          <ClearButton clearContact={this.clearContact} />
+      </div>
+      <div>
+        {contacts.length ? <h2>Contacts</h2> : ''}
+        {contacts.length > 1 ? (
+          <Filter value={filter} filterContacts={filterContacts} />
         ) : (
           ''
         )}
-        <p className={s.totalContacts}>
-          Total Contacts:
-          <span className={s.totalNumber}>{visibleContacts.length}</span>
-        </p>
+        {contacts.length ? (
+          <ContactList
+            visibleContacts={visibleContacts}
+            deleteContact={deleteContact}
+            onClickItem={onClickItem}
+          />
+        ) : (
+          ''
+        )}
       </div>
-    );
-  }
+      {(contacts.length === visibleContacts.length) &
+      (visibleContacts.length > 1) ? (
+        <ClearButton clearContact={clearContact} />
+      ) : (
+        ''
+      )}
+      <p className={s.totalContacts}>
+        Total Contacts:
+        <span className={s.totalNumber}>{visibleContacts.length}</span>
+      </p>
+    </div>
+  );
 }
 
 export default App;
--- src/components/DoForm/DoForm.jsx
+++ src/components/DoForm/DoForm.jsx
@@ -1,73 +1,70 @@
 import propTypes from 'prop-types';
 import s from './DoForm.module.css';
 import { nanoid } from 'nanoid';
-const { Component } = require('react');
-class DoForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
-  handleChange = e => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
+const { Component, useState } = require('react');
+function DoForm({ addContact }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleChange = e => {
+    if (e.target.name === 'name') {
+      setName(e.target.value);
+    } else if (e.target.name === 'number') setNumber(e.target.value);
   };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
     const contact = {
-      name: this.state.name,
-      number: this.state.number,
+      name,
+      number,
       id: nanoid(),
     };
-    this.props.addContact(contact);
-    this.setState({ name: '', number: '' });
+    addContact(contact);
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    const { number, name } = this.state;
-    return (
-      <form className={s.form} action="name" onSubmit={this.handleSubmit}>
-        <div className={s.nameBox}>
-          <label>
-            <span className={s.nameInput}>Name</span>
-            <input
-              className={s.input}
-              type="text"
-              name="name"
-              placeholder="Enter name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-              onChange={e => this.handleChange(e)}
-              value={name}
-            />
-          </label>
-        </div>
+  return (
+    <form className={s.form} action="name" onSubmit={handleSubmit}>
+      <div className={s.nameBox}>
+        <label>
+          <span className={s.nameInput}>Name</span>
+          <input
+            className={s.input}
+            type="text"
+            name="name"
+            placeholder="Enter name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            onChange={e => handleChange(e)}
+            value={name}
+          />
+        </label>
+      </div>
 
-        <div className={s.phoneBox}>
-          <label>
-            <span className={s.nameInput}>Phone</span>
-            <input
-              className={s.input}
-              type="tel"
-              name="number"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              placeholder="Enter number phone"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-              onChange={e => this.handleChange(e)}
-              value={number}
-            />
-          </label>
-        </div>
-        <button className={s.addButton}>Add contact</button>
-      </form>
-    );
-  }
+      <div className={s.phoneBox}>
+        <label>
+          <span className={s.nameInput}>Phone</span>
+          <input
+            className={s.input}
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            placeholder="Enter number phone"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            onChange={e => handleChange(e)}
+            value={number}
+          />
+        </label>
+      </div>
+      <button className={s.addButton}>Add contact</button>
+    </form>
+  );
 }
 
 DoForm.propTypes = {
-  contacts: propTypes.array.isRequired,
   addContact: propTypes.func.isRequired,
 };
 
